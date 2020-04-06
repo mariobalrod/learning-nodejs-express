67db4cabafb4b9847238721f112fde5a4939973e
mariobalrod <marioballestero.rdg@gmail.com>
2020-04-06 18:09:58 +0200

    Añadiendo static files y cambiando configuracion del motor de plantillas ejs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 const express = require('express');
-const server = express();
-const ejs = require('ejs');
-
 /*
     Para unir directorios path.join(),
     evita conflictos con directs de 
@@ -9,16 +6,21 @@ const ejs = require('ejs');
 */
 const path = require('path');
 
+const server = express();
+
 // Server Settings
-server.set('port', 8080);
+server.set('port', process.env.PORT || 3000);
     //_dirname : nos da la ruta absoluta
 server.set('views', path.join(__dirname, 'views'));
-server.set('view engine', ejs);
+server.set('view engine', 'ejs');
     //configuracion del motor de plantillas para usar .html en vez de .ejs
-server.engine('html', ejs.renderFile);
+//server.engine('html', ejs.renderFile);
 
 // Routes
-server.use(require('./routes/index'));
+server.use(require('./routes'));
+
+// Static Files
+server.use(express.static(path.join(__dirname, 'public')));
 
 // Listening the server
 server.listen(server.get('port'), () => {
